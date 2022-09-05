d18ff9137516f3711a9725dedb2632230bf66b9f
Lewandodev <mlewandowski380@gmail.com>
2022-09-05 23:59:46 +0200

    Advanced qucik sort

---
+++ Advanced quick sort.py
@@ -0,0 +1,9 @@
+#Advanced qucik sort is combination of 2 algorithms: quick sort and insertion sort
+#combination of qucik and insertion sort will create very efficient algorithm regardless of the size of input
+#with the time complexity of O(n^2)
+#(as insertion sort is better for small arrays and qucik sort for large arrays)
+
+
+
+#Algorithm will consist of auxiliary fucntions (qucik sort and insertion sort) and the main advanced sort algorithm
+#the main idea of the algorithm will be recursively checking the size of input array and deciding whether we will use qucik sort or insertion sort
