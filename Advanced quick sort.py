ecfbfca1eaa0c1a9041b9e45f6d70771c2432e48
Lewandodev <mlewandowski380@gmail.com>
2022-09-06 00:10:29 +0200

    Advanced quick sort algorithm

--- Advanced quick sort.py
+++ Advanced quick sort.py
@@ -6,4 +6,9 @@
 
 
 #Algorithm will consist of auxiliary fucntions (qucik sort and insertion sort) and the main advanced sort algorithm
-#the main idea of the algorithm will be recursively checking the size of input array and deciding whether we will use qucik sort or insertion sort
+#the main idea of the algorithm will be recursively checking the size of input array and deciding whether we will use qucik sort or insertion sort on array portion
+#according to some threshold value, since insertion sort will be implemented its best to set the threshold value to a small number because insertion sort works best
+#for such arrays
+
+
+#we will need to modify both insertion and qucik sort functions for this function, each of the functions will have 3 values
