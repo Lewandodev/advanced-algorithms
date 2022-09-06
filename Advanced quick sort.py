8143db373e5cb45438adaf6181cafb1c2ebce956
Lewandodev <mlewandowski380@gmail.com>
2022-09-06 23:50:02 +0200

    Advanced quick sort algorithm

--- Advanced quick sort.py
+++ Advanced quick sort.py
@@ -11,4 +11,24 @@
 #for such arrays
 
 
-#we will need to modify both insertion and qucik sort functions for this function, each of the functions will have 3 values
+#we will need to modify both insertion and qucik sort functions for this advamced algorithm to work, each of the functions will have 3 values
+
+
+#modified insertion sort
+def insertion_sort(arr,prime,n): #arr is array, prime is the first index, n is size of array but value we will input will be the last index
+    for i in range(prime+1,n+1):
+        val=arr[i]
+        j=i #the "pivot value" of outer loop which checks others
+
+        while j>prime and arr[j-1]>val:
+            arr[j]=arr[j-1]
+            j-=1
+        arr[j]=val
+
+    return arr
+
+aba=[5,3,6,9,1,2,4]
+#qucik check if modified insertion sort works
+print(aba)
+print('insertion sort',insertion_sort(aba,0,6))#or len(aba)-1
+
