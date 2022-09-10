a04dce91448a0473875ebfa8a682f18ded859f67
Lewandodev <mlewandowski380@gmail.com>
2022-09-11 00:00:13 +0200

    Bellman-Ford algorithm

---
+++ Bellman-Ford algorithm.py
@@ -0,0 +1,6 @@
+#Bellman Ford algorithm used on graph problem of "Cheapest flights within K stops"
+
+#Problem description:
+#There are n cities connected by some number of flights. You have been given an array "flights" where flights[i]=[from(i),to(i),price(i)] indicates
+#that there is a flight from city from(i) to city to(i) with cost price(i).
+#You are also given three integers "src","dst" and "k" return the ""cheapest"" price from 'src' to "dst"with at most k stops.
