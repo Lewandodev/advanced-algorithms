f5f8039f7a9542d304d65ececf4d6470755229be
Lewandodev <mlewandowski380@gmail.com>
2022-09-20 23:55:45 +0200

    Dijkstra algorithm Both versions and commentary in English

--- Dijkstra algorithm.py
+++ Dijkstra algorithm.py
@@ -31,30 +31,55 @@
 
 #Dijkstra algorithm in code:
 #regardless of implementation we will use dictionaries
-graph={}
+graph={'a':{'b':10,'c':3},'b':{'c':1,'d':2},'c':{'b':4,'d':8,'e':2},'d':{'e':7},'e':{'d':9}}
 
 #simpler version
 #we use graph that is initally defined
 def dijkstra_s(graph,start,goal):
     shortest_distance={} #will be updated
     prior={} #used to find shortest path
-    inv_nodes=graph #nodes we wont see we will be running through them until the are all seen
+    uns_nodes=graph #nodes we wont see we will be running through them until the are all seen as our function is iterative
+    infi=float("inf")
+    path=[]
+
+    #setting all our distances expect start to infinity
+    for node in uns_nodes:
+        shortest_distance[node]=infi
+    shortest_distance[start]=0
+
+    while uns_nodes: #we will be iterating till there's nothing in uns_nodes
+        min_node=None
+        for node in uns_nodes:
+            if min_node==None:
+                min_node=node
+            elif shortest_distance[node]<shortest_distance[min_node]:
+                min_node=node
+
+        for child_node,weight in graph[min_node].items():
+            if weight+shortest_distance[min_node]<shortest_distance[child_node]:
+                #relax shortest distance at the child node
+                shortest_distance[child_node]=weight+shortest_distance[min_node]
+                prior[child_node]=min_node
+        uns_nodes.pop(min_node)
+
+    #run throguh path backwards and write each step
+    current_node=goal
+    while current_node!=start:
+        #exception
+        try:
+            path.insert(0,current_node)
+            current_node=prior[current_node]
+        except KeyError:
+            print('Path not reachable')
+            break
+    if  shortest_distance[goal]==infi:
+        a=print('Shortest distance i',shortest_distance[goal])
+        b=print('\nPath',str(path))
+        return a,b
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-#shorter version more advanced version
+#shorter more advanced version
 #graph will be implemented later in the function
 def dijkstra_a(nodes,edges,source_index=0):
     path_lenghts={v: float('inf') for v in nodes} #Initially each path length is assigned to infinity expect source node
